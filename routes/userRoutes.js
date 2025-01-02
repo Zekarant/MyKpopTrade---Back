5e18f09af4788cf14eb067dd793a19051f9e28e0
CHOMEL <colin42660@gmail.com>
2025-01-02 05:04:34 +0100

    feat(dev): Add Typescript and gitignore

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addEmail } = require('../controllers/UserEmailController');
-
-const router = express.Router();
-
-router.post('/add-email', addEmail);
-
-module.exports = router;
